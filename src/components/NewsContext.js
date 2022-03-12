f044e7b37d4ab89c2a637465e3c6a5c445543d2b
AlexGechovski <aleksg00077@gmail.com>
2022-03-12 02:10:09 +0200

    Adding news api

---
+++ src/components/NewsContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "98eb3932f1b24473ad295d407f9b0f7c";
+
+  useEffect(() => {
+    axios
+      .get(
+        `
+        https://newsapi.org/v2/everything?q=space&apiKey=${apiKey}`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
--- src/components/newsPhoto.js
+++ src/components/newsPhoto.js
@@ -1,12 +1,17 @@
 import { Parallax } from 'react-parallax';
 import NewsPhoto from '../images/newsPhoto.jpg'
-import NewsBox from './newsBox';
-import React from 'react';
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+import { NewsContextProvider } from "./NewsContext";
+import News from "./News";
+import "./app.css";
 
-const News = () => (
+const NewsBackground = () => (
     <Parallax className='Background' bgImage={NewsPhoto} strength={800}>
-        <NewsBox ></NewsBox>
+        <NewsContextProvider>
+            <News />
+        </NewsContextProvider>
     </Parallax>
 );
 
-export default News;
+export default NewsBackground;
